9a5e906a3df584cc84cb3027e72915a9bec11d5f
andrei-229 <andrejbelak708@gmail.com>
2021-12-25 21:25:50 +0300

    Добавление наброска меню

---
+++ startMenu.py
@@ -0,0 +1,50 @@
+import pygame
+import pygame_gui
+
+pygame.init()
+
+pygame.display.set_caption('Quick Start')
+window_surface = pygame.display.set_mode((800, 600))
+
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('Blue'))
+
+clock = pygame.time.Clock()
+
+manager = pygame_gui.UIManager((800, 600))
+levels_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                             text='Levels',
+                                             manager=manager)
+
+settings_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 335), (100, 50)),
+                                               text='Settings',
+                                               manager=manager)
+
+st = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((250, 115), (200, 100)),
+                                   html_text='<font size = 5>Menu</font>',
+                                   manager=manager,
+                                   wrap_to_height=True)
+
+is_running = True
+
+while is_running:
+    time_delta = clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == levels_button:
+                    print('Open menu levels')
+                elif event.ui_element == settings_button:
+                    print('Open settings')
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+    pygame.display.update()
